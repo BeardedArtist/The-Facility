c03ddd66456dac45411eb90a8d2437e0b829eb36
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-04-11 23:11:47 +0900

    Added AI

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; // Make sure to always have this for AI
+
+public class AIController : MonoBehaviour
+{
+    public Transform playerTransform;
+    NavMeshAgent agent;
+    Animator animator;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = playerTransform.position;
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+}
--- Assets/Scripts/DoorInteraction.cs
+++ Assets/Scripts/DoorInteraction.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class DoorInteraction : MonoBehaviour
 {
-    [SerializeField] private bool triggerActive = false;
+    public Transform slidingDoor;
+    [SerializeField] private float doorSpeed = 1f;
+    [SerializeField] private float sizeOfDoorInX = 1f;
+    [SerializeField] private float amountOfDoorSlided = 0.9f;
+
+    private Vector3 slidingDoorCloseTarget;
+    private Vector3 slidingDoorOpenTarget;
+    private float startTime;
+    private float totalDistanceToCover;
+    private bool isDoorCurrentlyOpen = false;
+    private bool isDoorCurrentlyClosed = false;
     [SerializeField] private bool isDoorOpen = false;
-    [SerializeField] private int doorSpeed = 10;
-
-
-    Vector3 startPosition;
-    Vector3 endPosition;
-    Vector3 difference;
+    [SerializeField] private bool triggerActive = false;
 
     public void OnTriggerEnter(Collider other) 
     {
@@ -30,29 +35,62 @@ public class DoorInteraction : MonoBehaviour
         }
     }
 
+
+    private void Start() 
+    {
+        SetInitialReferences();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (triggerActive && isDoorOpen == false && Input.GetKeyDown(KeyCode.E))
         {
+            
             OpenDoor();
-            isDoorOpen = true;
         }
         
         else if (triggerActive && isDoorOpen == true && Input.GetKeyDown(KeyCode.E))
         {
+            
             CloseDoor();
-            isDoorOpen = false;
         }
     }
 
     public void OpenDoor()
     {
-        transform.Translate(-2,0,0);
+        //startTime = Time.time;
+        float distanceCovered = (Time.time - startTime) * doorSpeed;
+        float fractionOfJourney = distanceCovered / totalDistanceToCover;
+        slidingDoor.localPosition = Vector3.Lerp(slidingDoor.localPosition, slidingDoorOpenTarget, fractionOfJourney);
+
+        if (Mathf.Approximately(slidingDoor.localPosition.x, slidingDoorOpenTarget.x))
+        {
+            Debug.Log("Doors Opened");
+            isDoorOpen = true;
+        }
     }
 
     public void CloseDoor()
     {
-        transform.Translate(2,0,0);
+        //startTime = Time.time;
+        float distanceCovered = (Time.time - startTime) * doorSpeed;
+        float fractionOfJourney = distanceCovered / totalDistanceToCover;
+        slidingDoor.localPosition = Vector3.Lerp(slidingDoor.localPosition, slidingDoorCloseTarget, fractionOfJourney);
+
+        if (Mathf.Approximately(slidingDoor.localPosition.x, slidingDoorCloseTarget.x))
+        {
+            Debug.Log("Doors Closed");
+            isDoorOpen = false;
+        }
+    }
+
+    void SetInitialReferences()
+    {
+        slidingDoorCloseTarget = slidingDoor.localPosition;
+
+        slidingDoorOpenTarget = new Vector3(slidingDoor.localPosition.x - (sizeOfDoorInX * amountOfDoorSlided), slidingDoor.localPosition.y, slidingDoor.localPosition.z);
+
+        totalDistanceToCover = Vector3.Distance(slidingDoorCloseTarget, slidingDoorOpenTarget);
     }
 }
