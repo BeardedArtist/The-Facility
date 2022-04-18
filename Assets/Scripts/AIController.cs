e7901c1c6d86538cea17eb7df55edbb5e37e7018
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-04-18 15:42:10 +0900

    Fixed a detection bug in AI

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -12,6 +12,8 @@ public class AIController : MonoBehaviour
     private string state = "idle";
     private bool alive = true;
     private float wait = 0f;
+    private bool highAlert = false;
+    private float alertTime = 50f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +29,7 @@ public class AIController : MonoBehaviour
             RaycastHit rayHit;
             if (Physics.Linecast(eyes.position, playerTransform.position, out rayHit)) // checks to see if the raycast intersects with anything
             {
-                
+                //print("hit " + rayHit.collider.gameObject.name);
                 if (rayHit.collider.gameObject.name == "FirstPersonPlayer")
                 {
                     print("hit " + rayHit.collider.gameObject.name);
@@ -35,7 +37,7 @@ public class AIController : MonoBehaviour
                     if (state != "kill")
                     {
                         state = "chase";
-                        //agent.speed = 5f;
+                        agent.speed = 5f;
                     }
                 }
             } 
@@ -52,9 +54,25 @@ public class AIController : MonoBehaviour
             if (state == "idle")
             {
                 // pick random location to walk to
-                Vector3 randomPosition = Random.insideUnitSphere * 50f;
+                Vector3 randomPosition = Random.insideUnitSphere * alertTime;
                 NavMeshHit navHit;
                 NavMesh.SamplePosition(transform.position + randomPosition, out navHit, 50f, NavMesh.AllAreas); // finding a random spot from where the AI is standing on the NavMesh
+                
+                // search for player 
+                if (highAlert)
+                {
+                    NavMesh.SamplePosition(playerTransform.position + randomPosition, out navHit, 50f, NavMesh.AllAreas); // finding a random spot from where the AI is standing on the NavMesh
+                    // each time, lose awareness of player position
+                    alertTime += 5f;
+
+                    if (alertTime > 25f)
+                    {
+                        highAlert = false;
+                        alertTime = 50f;
+                        agent.speed = 3.5f;
+                    }
+                }
+                
                 agent.SetDestination(navHit.position); // setting the random position to the agent so it knows where to go. 
                 state = "walk";
             }
@@ -86,7 +104,27 @@ public class AIController : MonoBehaviour
             // chase
             if (state == "chase")
             {
-                agent.destination = playerTransform.position;    
+                agent.destination = playerTransform.position;
+
+                //lose sight of player
+                float distance = Vector3.Distance(transform.position, playerTransform.position);
+                if (distance > 10f)
+                {
+                    state = "find";
+                }
+            }
+
+            //find
+            if (state == "find")
+            {
+                if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+                {
+                    state = "search";
+                    wait = 5f;
+                    highAlert = true;
+                    alertTime = 5f;
+                    CheckSight();
+                }
             }
 
             // agent.destination = playerTransform.position; // this gives the AI the players position and tells it to walk to the player
