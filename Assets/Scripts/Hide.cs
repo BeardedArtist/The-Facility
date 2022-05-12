7f04a687a8ea8427c82033f6410965a9dd99e3a8
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-05-12 13:00:32 +0900

    Updated Hiding Closet

--- Assets/Scripts/Hide.cs
+++ Assets/Scripts/Hide.cs
@@ -6,7 +6,11 @@ public class Hide : MonoBehaviour
 {
     public GameObject player;
     public GameObject warpTarget;
+    public GameObject warpTarget_2;
     private CharacterController characterController;
+    [SerializeField] private bool isHiding = false;
+
+
     private void Start() 
     {
         characterController = player.GetComponent<CharacterController>();
@@ -21,15 +25,42 @@ public class Hide : MonoBehaviour
         {
             if (other.tag == "Player")
             {
-                if (Input.GetKey(KeyCode.E))
+                Debug.Log("Player is ready to hide");
+
+                if (isHiding == false)
                 {
-                    Debug.Log("Player is read to hide");
-                    characterController.enabled = false;
-                    player.transform.position = warpTarget.transform.position;
-                    player.transform.rotation = warpTarget.transform.rotation;
-                    characterController.enabled = true;
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        Debug.Log("Player is hiding!");
+
+                        // PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
+                        // PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
+                        // PlayerPrefs.SetFloat("PlayerZ", player.transform.position.z);
+
+                        characterController.enabled = false;
+                        player.transform.position = warpTarget.transform.position;
+                        player.transform.rotation = warpTarget.transform.rotation;
+                        characterController.enabled = true;
+
+                        isHiding = true;
+                    }
                 }
-                
+
+                if (isHiding == true)
+                {
+                    if (Input.GetKeyDown(KeyCode.Q))
+                    {
+                        Debug.Log("Player is not hiding anymore");
+
+                        characterController.enabled = false;
+                        player.transform.position = warpTarget_2.transform.position;
+                        player.transform.rotation = warpTarget.transform.rotation;
+                        characterController.enabled = true;
+                        //player.transform.position = new Vector3(PlayerPrefs.GetFloat("PlayerX"), PlayerPrefs.GetFloat("PlayerY"), PlayerPrefs.GetFloat("PlayerZ"));
+
+                        isHiding = false;
+                    }                  
+                }
             }
         }
 
