712b2cda4b3c1d5e3bea3f6fbef7147593d98b6f
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-07-28 12:40:21 +0900

    Updating new scene with functionality

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -29,4 +29,24 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         playerBody.Rotate(Vector3.up * mouseX); // allows for players to look left/right w/mouse.
     }
+
+    // float xRotation = 0f;
+    // public Vector2 turn;
+    // public float sensitivity = 50;
+    // public Vector3 deltaMove;
+    // public float speed = 1;
+
+    // private void Start() 
+    // {
+    //     Cursor.lockState = CursorLockMode.Locked;    
+    // }
+
+    // private void Update() 
+    // {
+    //     turn.x += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+    //     turn.y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+    //     xRotation -= turn.y;
+    //     xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    //     transform.localRotation = Quaternion.Euler(xRotation, turn.x, 0);
+    // }
 }
