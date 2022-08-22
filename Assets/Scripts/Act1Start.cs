fb85a8167b9e0098bed619b13ca468c050c3c15f
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-08-22 21:39:17 +0900

    Setting up starting scene

--- Assets/Scripts/Act1Start.cs
+++ Assets/Scripts/Act1Start.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Act1Start : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject PCMessage;
+    //[SerializeField] private GameObject wakeUp_Anim;
+    [SerializeField] private Animator wakeUp_Anim;
+    [SerializeField] private CharacterController characterController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = player.GetComponent<CharacterController>();
+        wakeUp_Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void StartActOne()
+    {
+        characterController.enabled = false;
+        wakeUp_Anim.Play("CompleteBlackOut", 0, 0.0f);
     }
 }
