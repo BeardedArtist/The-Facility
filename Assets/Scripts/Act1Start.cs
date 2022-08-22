1cdf6def5e6a57074456dc443fb4668a016c9095
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-08-22 19:53:17 +0900

    Adding New Screen to Computer & Text

---
+++ Assets/Scripts/Act1Start.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Act1Start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
