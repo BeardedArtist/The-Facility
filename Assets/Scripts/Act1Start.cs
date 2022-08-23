e08c59d23e4710b6bcc73ba5256876c6cd5e47ef
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-08-23 11:31:42 +0900

    Setting up starting scene

--- Assets/Scripts/Act1Start.cs
+++ Assets/Scripts/Act1Start.cs
@@ -6,27 +6,54 @@ using UnityEngine.UI;
 public class Act1Start : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private GameObject wakeUpAnimObject;
     [SerializeField] private GameObject PCMessage;
+    [SerializeField] private GameObject getUpMessage;
     //[SerializeField] private GameObject wakeUp_Anim;
     [SerializeField] private Animator wakeUp_Anim;
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private MouseLook mouseLook;
+
+    private bool canGetup = false;
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = player.GetComponent<CharacterController>();
-        wakeUp_Anim = GetComponent<Animator>();
+        mouseLook = playerCamera.GetComponent<MouseLook>();
+        wakeUp_Anim = wakeUpAnimObject.GetComponent<Animator>();
+
+        StartActOne();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(GetUpMessage());
+
+        if (canGetup == true)
+        {
+            if (Input.anyKey)
+            {
+                Destroy(getUpMessage);
+                characterController.enabled = true;
+                mouseLook.enabled = true;
+            }
+        }
     }
 
     void StartActOne()
     {
         characterController.enabled = false;
+        mouseLook.enabled = false;
         wakeUp_Anim.Play("CompleteBlackOut", 0, 0.0f);
+    }
+
+    private IEnumerator GetUpMessage()
+    {
+        yield return new WaitForSeconds(5.0f);
+        getUpMessage.SetActive(true);
+        canGetup = true;
     }
 }
