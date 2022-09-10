0c2d3bafceeba28ed38c3fa76a25bbb10c4ac25b
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-09-10 11:09:27 +0900

    Main Circuit Function added

--- Assets/Scripts/CircuitBreakerManager.cs
+++ Assets/Scripts/CircuitBreakerManager.cs
@@ -6,8 +6,45 @@ public class CircuitBreakerManager : MonoBehaviour
 {
     [SerializeField] private int circuitBreakerOn;
 
+    // For Main Circuit Breaker Lights
+    [SerializeField] private GameObject mainCircuit_1;
+    [SerializeField] private GameObject mainCircuit_2;
+    [SerializeField] private GameObject mainCircuit_3;
+
+    [SerializeField] private Light pLight;
+    private Color colorGreen = Color.green;
+
+    [SerializeField] private Material greenMat;
+    // -------------------------
+
+    private void Update() 
+    {
+        turnOnMainCircuitLights();    
+    }
+
     public void IncrementNumber()
     {
         circuitBreakerOn++;
+    }
+
+    void turnOnMainCircuitLights()
+    {
+        if (circuitBreakerOn == 1)
+        {
+            mainCircuit_1.GetComponent<MeshRenderer>().material = greenMat;
+            pLight.color = colorGreen;
+        }
+
+        if (circuitBreakerOn == 2)
+        {
+            mainCircuit_1.GetComponent<MeshRenderer>().material = greenMat;
+            pLight.color = colorGreen;
+        }
+
+        if (circuitBreakerOn == 3)
+        {
+            mainCircuit_1.GetComponent<MeshRenderer>().material = greenMat;
+            pLight.color = colorGreen;
+        }
     }
 }
