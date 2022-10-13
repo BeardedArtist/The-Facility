3bb04cefb8b8509f183c7e6d0d4bd430f53be28b
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-10-13 17:33:31 +0900

    Added Blinking Function

---
+++ Assets/Scripts/Player/Blink.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    [SerializeField] private GameObject topLid;
+    [SerializeField] private GameObject bottomLid;
+    [SerializeField] private Animator blink_Anim;
+    [SerializeField] private Animator blink_Anim_2;
+    [SerializeField] bool isBlinking;
+    [SerializeField] private float timer;
+
+    private void Start() 
+    {
+        timer = Random.Range(4.0f, 10.0f);    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            timer = Random.Range(4.0f, 10.0f);
+            isBlinking = true;
+        }
+
+        if (isBlinking == true)
+        {
+            blink_Anim.Play("TopLidBlink", 0, 0.25f);
+            blink_Anim_2.Play("BottomLidBlink", 0, 0.25f);
+            isBlinking = false;
+        }
+    }
+}
