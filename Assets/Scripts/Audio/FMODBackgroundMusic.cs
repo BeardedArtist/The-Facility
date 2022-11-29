bfb957dbf326e4fa46b6edf98f73added766179c
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2022-11-29 18:53:58 -0500

    FMOD research

---
+++ Assets/Scripts/Audio/FMODBackgroundMusic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODBackgroundMusic : MonoBehaviour
+{
+    private static FMOD.Studio.EventInstance Music;
+
+    void Start()
+    {
+        Music = FMODUnity.RuntimeManager.CreateInstance("event:/Level Music/LEVEL_MUSIC");
+        Music.start(); // Disable this later
+        Music.release();
+    }
+
+    public void Progress (float ProgressLevel)
+    {
+        Music.setParameterByName("Progress", ProgressLevel);
+    }
+
+
+    private void OnDestroy() // Music will fade out if scene changes
+    {
+        Music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);    
+    }
+}
